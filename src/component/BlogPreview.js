ced2b6e516de1f16a6d42273f079ea1ed66b23a5
kikuishimoto <kikuish@gmail.com>
2020-11-18 19:00:25 -0800

    fixed bug

--- src/component/BlogPreview.js
+++ src/component/BlogPreview.js
@@ -1,13 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import BlogCard from './BlogCard';
 export default function BlogPreview() {
-    const [blogPosts, setBlogPosts] = useState("[]")
+    const [blogPosts, setBlogPosts] = useState("")
 
     useEffect(() => {
         fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@kikucodes")
         .then(res => res.json())
         .then(res => {
-            debugger
             setBlogPosts(res.items)
         })
         
@@ -16,7 +15,7 @@ export default function BlogPreview() {
     if (blogPosts.length) {
         let posts = blogPosts.slice(0,3);
         return posts.map((post) => {
-            return <BlogCard title={post.title} image={post.image} link={post.link}/>
+            return <BlogCard title={post.title} image={post.thumbnail} link={post.link}/>
         }
            
         )
